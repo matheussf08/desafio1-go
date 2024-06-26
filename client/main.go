38690be19ba19b506b2dde78c47b2243633f2c87
Matheus Faria <matheus_santana99@outlook.com>
2024-06-26 20:19:16 -0300

    Desafio 1 de Golang

---
+++ client/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+type Exchange struct {
+	Code       string `json:"code"`
+	Codein     string `json:"codein"`
+	Name       string `json:"name"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	VarBid     string `json:"varBid"`
+	PctChange  string `json:"pctChange"`
+	Bid        string `json:"bid"`
+	Ask        string `json:"ask"`
+	Timestamp  string `json:"timestamp"`
+	CreateDate string `json:"create_date"`
+} 
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 300 * time.Millisecond)
+	defer cancel()
+
+	request, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		log.Print(err.Error())
+	}
+	
+	response, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		msg := err.Error()
+
+		// Verifica se o erro é do prazo do context que excedeu
+		if strings.Contains(err.Error(), "context deadline exceeded") {
+			msg = "context deadline exceed"
+		}
+		log.Print(msg)
+	}
+
+	if response != nil {
+		defer response.Body.Close()
+	
+		body, err := io.ReadAll(response.Body)
+		if err != nil {
+			log.Print(err.Error())
+		}
+
+		var exchange Exchange
+		err = json.Unmarshal(body, &exchange)
+
+		if err != nil {
+			log.Print(err.Error())
+		}
+
+		file, err := os.Create("cotacao.txt")
+		if err != nil {
+			log.Print(err.Error())
+		}
+
+		_, err = file.WriteString("Dolar: {"+exchange.Bid+"}" )
+		if err != nil {
+			log.Print(err.Error())
+		}
+		file.Close()
+	}
+}
---
+++ server/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Currency struct {
+	UsdBrl Exchange
+}
+
+type Exchange struct {
+	Code       string `json:"code"`
+	Codein     string `json:"codein"`
+	Name       string `json:"name"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	VarBid     string `json:"varBid"`
+	PctChange  string `json:"pctChange"`
+	Bid        string `json:"bid"`
+	Ask        string `json:"ask"`
+	Timestamp  string `json:"timestamp"`
+	CreateDate string `json:"create_date"`
+} 
+
+type ExchangeDb struct {
+	Bid string
+}
+
+func main() {
+	http.HandleFunc("/cotacao", handlerExchange)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handlerExchange(w http.ResponseWriter, r *http.Request)  {
+	db, err := sql.Open("sqlite3", "./exchange.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// // Create table
+	// sqlStmt := `create table exchanges (id integer not null primary key autoincrement, bid text, create_date datetime default current_timestamp);`
+	// _, err = db.Exec(sqlStmt)
+	// if err != nil {
+	// 	log.Printf("%q: %s\n", err, sqlStmt)
+	// 	return
+	// }
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 200 * time.Millisecond)
+	defer cancel()
+
+	exchange, error := getDollarExchange(ctx)
+
+	if error != nil {
+		msg := error.Error()
+		// Verifica se o erro é do prazo do context que excedeu
+		if strings.Contains(error.Error(), "context deadline exceeded") {
+			msg = "context deadline exceed"
+		}
+		
+		log.Print(msg)
+		
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	// Insert bid into table
+	if exchange != nil && exchange.Bid != "" {
+		ctx2, cancel := context.WithTimeout(ctx, 10 * time.Millisecond)
+		defer cancel()
+		
+		stmt, err := db.Prepare("insert into exchanges(bid) values(?)")
+		if err != nil {
+			log.Print(error.Error())
+		}
+		defer stmt.Close()
+
+		_, err = stmt.ExecContext(ctx2, exchange.Bid)
+		if err !=  nil {
+			msg := err.Error()
+			// // Verifica se o erro é do prazo do context que excedeu
+			if strings.Contains(err.Error(), "context deadline exceeded") {
+				msg = "database context deadline exceed"
+			}
+
+			log.Print(msg)
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(exchange)
+}
+
+func getDollarExchange(ctx context.Context) (*Exchange, error) {
+	request, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	
+	var c Currency
+	err = json.Unmarshal(body, &c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c.UsdBrl, nil
+}
